e547c2c201f7233295809a6a3694ce53dd9880cd
Paddy Mariage <paddy.mariage@hotmail.fr>
2020-07-15 23:25:32 +0200

    fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,49 +1,23 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo : 'login',
-    pathMatch : 'full'
-    // similaire à : loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule),
-    //loadChildren: './pages/login/login.module#LoginPageModule'
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'tabs',
-    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
-    //loadChildren: './pages/tabs/tabs-routing.module#TabsPageRoutingModule'
-  },
-  {
-    path: 'settings',
-    loadChildren: () => import('./pages/settings/settings.module').then( m => m.SettingsPageModule)
-    // loadChildren: './pages/settings/settings.module#SettingsPageModule'
-  },  
-  {
-    path: 'documents',
-    loadChildren: () => import('./pages/documents/documents.module').then( m => m.DocumentsPageModule)
-    //loadChildren: './pages/documents/documents.module#DocumentsPageModule'
-  },
-  {
-    path: 'produits',
-    loadChildren: () => import('./pages/produits/produits.module').then( m => m.ProduitsPageModule)
-    //loadChildren: './pages/documents/documents.module#DocumentsPageModule'
-  }
-  /*{
-    path: 'contact',
-    loadChildren: () => import('./pages/contact/contact.module').then( m => m.ContactPageModule)
-
-  }*/
+    {
+        path: '',
+        loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+    },
+    {
+        path: 'login',
+        loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule),
+        pathMatch: 'full'
+    }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
+    ],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -1,55 +1,40 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {TabsPage} from "./tabs.page";
 
 const routes: Routes = [
-  {
-        path: '',
-        component: TabsPage,
-        children:[
-            {
-              path:'produits',
-              children: [
-              {
-                path: '',
-                loadChildren: () => import('../produits/produits.module').then( m => m.ProduitsPageModule)
-              }
-          ]
-        },
-          {
-            path:'historique',
-            children: [
-              {
-                path:'',
-                loadChildren: () => import('../historique/historique.module').then( m => m.HistoriquePageModule)
-            }
-          ]
-          },
-          {
-            path:'documents',
-            children: [
-              {
-                path:'',
-                loadChildren: () => import('../documents/documents.module').then( m => m.DocumentsPageModule)
-            }
-          ]
-          },
-          {
-            path: '',
-            redirectTo: '/tabs/produits',
-            pathMatch: 'full'
-          }
-        ]
-     },
-     {
-     path:'',
-     redirectTo:'/tabs/produits',
-     pathMatch:'full'
+    {
+        path: '',
+        component: TabsPage,
+        children: [
+            {
+                path: 'article',
+                loadChildren: () => import('../article/article.module').then(m => m.ProduitsPageModule)
+            },
+            {
+                path: 'historique',
+                loadChildren: () => import('../historique/historique.module').then(m => m.HistoriquePageModule)
+            },
+            {
+                path: 'documents',
+                loadChildren: () => import('../documents/documents.module').then(m => m.DocumentsPageModule)
+            },
+            {
+                path:'',
+                redirectTo:'/article'
+            }
+        ]
+    },
+    {
+        path: '',
+        redirectTo: '/article',
+        pathMatch: 'full'
     }
-  ];
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
-export class TabsPageRoutingModule {}
+export class TabsPageRoutingModule {
+}
