0663125e4b076b4cce2af172e00414a5b709076f
Paddy Mariage <paddy.mariage@hotmail.fr>
2020-08-05 12:02:51 +0200

    removed modal on article & all commented code in article.page.ts

--- src/app/pages/article/article.page.ts
+++ src/app/pages/article/article.page.ts
@@ -51,69 +51,6 @@ export class ArticlePage implements OnInit {
         this.orderLineBackup = this.orderLineList;
     }
 
-    // Dés qu'une quantité est selectionné pour un produit, la méthode la transmet au service
-    // n'est plus utilisée car on utilise orderLine avec la quantité et non Article
-    // onChange(ev: any, art: Article) {
-    //     const val = ev.target.value;
-    //     this.totalQuantity = 0;
-    //
-    //     // on ajoute une line pour l'afficher dans la commande par la suite
-    //     const line = {
-    //         orderNumber: null,
-    //         article: art,
-    //         quantity: val
-    //     };
-    //     // S'il n'y a pas de lignes, on ajoute directement. S'il y en a, on remplace la quantité de la line par la nouvelle.
-    //     if (line.quantity === 0) { // suppression
-    //
-    //         if (this.cart.length !== 0) {
-    //             const index = this.getArticlePosition(line);
-    //             if (index !== -1) {
-    //                 this.cart.splice(index, 1);
-    //             }
-    //         }
-    //     } else { // ajout ou modif
-    //         const index: number = this.getArticlePosition(line);
-    //         if (index === -1) { // pas trouve donc on ajoute
-    //             console.log('entré dans ajout');
-    //             this.cart.push(line);
-    //         } else { // update
-    //             console.log('entré dans update');
-    //             this.cart[index] = line;
-    //         }
-    //     }
-    //     this.cartService.setCart(this.cart);
-    // }
-
-    // permet de retrouver la position d'un article à partir d'une ligne de commande
-    // n'est plus utilisée car on utilise l'objet orderLine plutôt que article
-    // getArticlePosition(ligne: OrderLine): number {
-    //     let found = false;
-    //     let index = 0;
-    //     while (!found && index < this.cart.length) {
-    //         if (this.cart[index].article === ligne.article) {
-    //             found = true;
-    //         }
-    //         index++;
-    //     }
-    //     if (found) {
-    //         return index - 1;
-    //     }
-    //     return -1;
-    // }
-
-    // quand on clique sur l'article, on affiche la description
-    // n'est plus utilisée car on utilise l'objet orderLine plutôt que article
-    // async createArticleDetails(articleData: Article) {
-    //     this.articleService.setArticle(articleData);
-    //     const modal = await this.modalController.create({
-    //         component: SingleArticlePage,
-    //         cssClass: 'modal-article',
-    //         backdropDismiss: true
-    //     });
-    //     return await modal.present();
-    // }
-
     // initialisation de la liste d'article créée. On utilisera le back à la place ici
     initializeArticles() {
         this.articleList = [{
@@ -329,25 +266,6 @@ export class ArticlePage implements OnInit {
 
     // méthode pour la searchbar de ionic.
     getArticleSearched(ev: any) {
-        // n'est plus utilisé car passage de la manipulation d'article à orderline
-        // on réinitialise la liste d'article a afficher en refaisant appel à la liste originelle
-        // this.initializeArticles();
-        //
-        // // set la valeur de l'input de la searchbar dans "val". On indique que c'est un input html
-        // const val = (ev.target as HTMLInputElement).value;
-        //
-        // // si rien n'est mis on affiche tout, sinon on filtre avec ce qui a été inséré
-        // if (val && val.trim() !== '') {
-        //     this.articleList = this.articleList.filter((article) => {
-        //         return (article.reference.toLowerCase().indexOf(val.toLowerCase()) > -1 ||
-        //             article.label.toLowerCase().indexOf(val.toLowerCase()) > -1);
-        //     });
-        // }
-
-        // on réinitialise la liste d'article a affiché en refaisant appel à la liste originelle
-        // this.initializeArticles();
-
-        // *****************************************
 
         // ici on récupère notre backup qu'on pourra manipuler dans un objet différent.
         let orderLines = this.getOrderLines();
